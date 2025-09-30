b40532a44b85dbb2c0308ed6285a90c30440ee16
pdthang2000 <pdthang2000@gmail.com>
2025-09-30 13:31:02 +0700

    clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,14 @@
 import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
-import * as process from 'process';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/filters/http-exception-filter.filter';
 import { TransformResponseInterceptor } from './common/interceptors/transform-response-interceptor.interceptor';
 import { FlashcardsModule } from './modules/flashcards/flashcards.module';
 config();
 @Module({
-  imports: [
-    MongooseModule.forRoot(process?.env?.DATABASE_URL),
-    FlashcardsModule,
-  ],
+  imports: [FlashcardsModule],
   controllers: [AppController],
   providers: [
     AppService,
@@ -30,7 +25,6 @@ config();
 export class AppModule implements OnModuleInit {
   async onModuleInit() {
     try {
-      MongooseModule.forRoot(process?.env?.DATABASE_URL);
       console.log('Connected to the database successfully!');
     } catch (error) {
       console.error('Failed to connect to the database:', error);
